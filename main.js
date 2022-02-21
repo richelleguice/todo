80e93e43bec1425cbc7a4b4712f2ee73e0832d9f
Richelle Guice <rguice@andrew.cmu.edu>
2022-02-21 11:08:37 -0500

    new commit

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ function addNewItem() {
     newItem.appendChild(document.createTextNode(itemInput.value));
     list.appendChild(newItem);
 }
+
+function deleteListItem(item) {
+    console.log(item);
+    // remove li element (item) from ol element (item.parentNode)
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+    item.parentNode.style.textDecoration = "line-through";
+    // in css, this would be: "text-decoration: line-through"
+    } else {
+    item.parentNode.style.textDecoration = "none";
+    // in css, this would be: "text-decoration: none"
+    }
+}
